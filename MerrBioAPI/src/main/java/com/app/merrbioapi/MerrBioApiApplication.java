c1a75ed40fd09fa360c45d06a2beee77b9600889
Ergys <ergysxhaollari02@gmail.com>
2025-04-12 10:20:18 +0200

    initial commit

---
+++ MerrBioAPI/src/main/java/com/app/merrbioapi/MerrBioApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.merrbioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerrBioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerrBioApiApplication.class, args);
+    }
+
+}
